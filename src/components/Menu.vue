32b22e0e0876a2504ae22c0e2d7346aaf8684a0d
ceciliehrr <ceciliehrr@gmail.com>
2022-01-01 23:41:48 +0100

    Add Element plus

---
+++ src/components/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-menu class="el-menu-vertical-demo" mode="vertical">
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon><setting /></el-icon>
+      </template>
+      <el-menu-item-group title="Group One">
+        <el-menu-item index="1-1">Fargekode 1 <Switch /></el-menu-item>
+        <el-menu-item index="1-2">Fargekode 2 <Switch /></el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+import { Setting } from "@element-plus/icons-vue";
+import Switch from "./Switch.vue";
+</script>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+}
+</style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+</script>
+
+  <template>
+  <el-image
+    style="border-radius: 50%; width: 100px; height: 100px"
+    :src="srcOne"
+  ></el-image>
+  <el-timeline>
+    <el-timeline-item timestamp="2018/4/12" placement="top" color="#ff715b">
+      <el-card>
+        <h4>Update Github template</h4>
+        <p>Tom committed 2018/4/12 20:46</p>
+      </el-card>
+    </el-timeline-item>
+    <el-timeline-item timestamp="2018/4/3" placement="top" color="#ff715b">
+      <el-card>
+        <h4>Update Github template</h4>
+        <p>Tom committed 2018/4/3 20:46</p>
+      </el-card>
+    </el-timeline-item>
+    <el-timeline-item timestamp="2018/4/2" placement="top" color="#ff715b">
+      <el-card>
+        <h4>Update Github template</h4>
+        <p>Tom committed 2018/4/2 20:46</p>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
+  <el-image
+    style="border-radius: 50%; width: 100px; height: 100px"
+    :src="srcTwo"
+  ></el-image>
+</template>
+<script lang="ts" setup>
+const srcOne = "../src/assets/image/chrPortrett2.jpg";
+const srcTwo = "../src/assets/image/cecilieliten.jpg";
+</script>
+<style scoped>
+.el-timeline {
+  padding-left: 0;
+  padding-top: 20px;
+}
+.el-image {
+  border: 5px solid #ff715b;
+}
+.el-card {
+  /*background-color: #462255;*/
+  background-color: white;
+}
+.el-timeline-element {
+  background-color: #ff715b;
+}
+</style>
